4d378fffdf07be4488110e1f2a62aab1ef328b81
Sadie Lombardi <slombardi@wpi.edu>
2021-09-28 16:43:25 -0400

    added minmax stuff

--- player/src/main_pkg/agent.java
+++ player/src/main_pkg/agent.java
@@ -3,6 +3,8 @@ package main_pkg;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -61,9 +63,93 @@ public class agent {
         return 0;
     }
 
-    public int min_max(){
-        int result = 0;
+    /**
+     * Min max function
+     * @return
+     */
+    public MinMaxMove min_max(board the_board, int depth, int max_depth){
+        /*
+        minimax(in game board, in int depth, in int max_depth,
+        out score chosen_score, out score chosen_move)
+  begin
+    if (depth = max_depth) then
+        chosen_score = evaluation(board);
+   ` else
+        moves_list = generate_moves(board);
+       ` if (moves_list = NULL) then
+        `    chosen_score = evaluation(board);
+       ` else
+            for (i = 1 to moves_list.length) do
+                best_score = infinity;
+                new_board = board;
+                `apply_move(new_board, moves_list[i]);
+                minimax(new_board, depth+1, max_depth, current_score, current_move);
+                if (better(current_score, best_score)) then
+                    best_score = current_score;
+                    best_move = current_move;
+                endif
+            enddo
+            chosen_score = best_score;
+            chosen_move = best_move;
+        endif
+    endif
+end.
+         */
+        int chosen_score = 0;
+        cell chosen_move = null;
+        int best_score = 0;
+        cell best_move = null;
 
-        return result;
+        List<cell> moves_list = new ArrayList<>();
+
+        if(depth == max_depth){
+            chosen_score = evaluation();
+        }else {
+            moves_list = generate_moves();
+            if (moves_list == null) {
+                chosen_score = evaluation();
+            }else{
+                for(int i = 1; i <moves_list.size();i++){
+                    best_score = 10000000;
+                    board new_board = null;
+
+                    apply_move(new_board, moves_list.get(i));
+
+                    min_max(new_board, depth+1, max_depth);
+
+                    if (better(chosen_score, best_score)){
+                        best_score = chosen_score;
+                        best_move = chosen_move;
+                    }
+
+                }
+                chosen_score = best_score;
+                chosen_move = best_move;
+            }
+        }
+
+        MinMaxMove returnMove = null;
+        returnMove.setMove(chosen_move);
+        returnMove.setScore(chosen_score);
+
+        return returnMove;
+    }
+
+    public int evaluation(){
+        return 0;
+    }
+
+    public List<cell> generate_moves(){
+        List<cell> moves_list = new ArrayList<>();
+        return moves_list;
+    }
+    public void apply_move(board new_board, cell cell){
+
+
+    }
+    public boolean better(int the_score, int best_score){
+
+        return true;
+
     }
 }
